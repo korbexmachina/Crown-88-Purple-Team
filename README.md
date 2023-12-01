0003d2db4e7a7b0ce3a48180ec3c5312c93f68cb
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-12-01 14:28:28 -0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Crown-88-Purple-Team
+
+## Research Questions
+
+## Hypothesis
+
+## Method
+
+## Results
+
+## Conclusion
+
+## Datasets
+
+- Billboard Hot 100
+- Spotify Million Song Dataset
---
+++ src/csv_parser.py
@@ -0,0 +1,7 @@
+# Purple Team CSV Parser
+# Reads in the Billboard Hot 100 Dataset and the Spotify Dataset
+# and creates a new dataset with the lyrics associated with each song
+
+import csv
+import os
+import sys

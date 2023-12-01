d0f4c5ac0e88bac0242df658e48d41f9b45af66d
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-12-01 15:47:08 -0800

    ready for collaboration

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -1,7 +1,14 @@
 # Purple Team CSV Parser
 # Reads in the Billboard Hot 100 Dataset and the Spotify Dataset
-# and creates a new dataset with the lyrics associated with each song
+# Creates a CSV file for each year with the following columns:
+#   - Song Name
+#   - Artist
+#   - Date (From charts.csv)
+#   - Lyrics
+# Only using Data from 2011-2021
 
 import csv
 import os
 import sys
+import pandas as pd
+

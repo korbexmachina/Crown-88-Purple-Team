71dedef3176b23a6cd75e22efbacf8ddaaf8c6fc
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-12-01 16:57:36 -0800

    working on interpreter

--- src/interpreter.py
+++ src/interpreter.py
@@ -13,7 +13,7 @@ import sys
 import csv
 
 # Global variables
-MODEL = "llama2"
+MODEL = "llama-lyric"
 PROMPT = "Catagorize this song based on the lyrics: happy, sad, angry, or calm"
 PORT = 11434
 HOST = "127.0.0.1"
@@ -24,14 +24,14 @@ def send_request(data) -> requests.Response:
     '''
     url = "http://" + HOST + ":" + str(PORT) + "/api/generate/"
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(url, data=json.dumps(format_request(data)), headers=headers)
+    r = requests.post(url, data=json.dumps(data), headers=headers)
     return r
 
-def format_request(data) -> dict:
+def format_request(song, artist, lyrics) -> dict:
     '''
     Formats the request to send to ollama
     '''
-    return {"model": MODEL, "prompt": data, "stream": False}
+    return {"model": MODEL, "prompt": (f"{song} by {artist}:\n{lyrics}"), "stream": False}
 
 def get_response(r) -> dict:
     '''
@@ -39,9 +39,9 @@ def get_response(r) -> dict:
     '''
     return r.json()
 
-def get_prediction(response) -> str:
+def get_output(response) -> str:
     '''
-    Gets the prediction from the response
+    Gets the output from the response
     '''
     return response["response"]
 
@@ -49,6 +49,9 @@ def main():
     '''
     Main function
     '''
+    # Start the timer
+    start = time.time()
+
     # Get the file name from the command line
     file_name = sys.argv[1]
     # Open the file
@@ -59,16 +62,26 @@ def main():
         next(reader)
         # For each row in the file
         for row in reader:
+            # Get the song name
+            song = row[1]
+            # Get the artist name
+            artist = row[2]
             # Get the lyrics
-            lyrics = row[5]
+            lyrics = row[3]
+            # Format the request
+            req = format_request(song, artist, lyrics)
             # Send the request to ollama
-            response = send_request(lyrics)
+            response = send_request(req)
             # Get the response
             response = get_response(response)
-            # Get the prediction
-            prediction = get_prediction(response)
-            # Print the prediction
-            print(prediction)
+            # Get the output
+            output = get_output(response)
+            # Print the output
+            print(output)
+            # Print the run time, the number of songs processed, and the average time per song
+            print(f"Run time: {time.time() - start} seconds")
+            print(f"Songs processed: {reader.line_num - 1}")
+            print(f"Average time per song: {(time.time() - start) / (reader.line_num - 1)} seconds")
 
 if __name__ == "__interpreter__":
     main()
